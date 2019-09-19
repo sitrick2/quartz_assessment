985476bc768ef1d45247d33930f02cf51ba2c419
David Sitrick <sitrick2@gmail.com>
2019-09-19 01:04:43 +0000

    css improvements

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -121,26 +121,14 @@
                 text-decoration: none;
                 margin-bottom: 20px;
                 padding: 20px;
-                display: inherit;
+                position: absolute;
+                margin-left: -275px;
+                margin-top: 40px;
             }
         </style>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
                 <div class="title m-b-md">
                     Quartz Assessment Quiz
